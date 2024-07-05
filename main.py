68a1c2399278fe086c95f2cf491cc0b4dc0e01fc
Sathvik Vittal <sathvik.vittal@gmail.com>
2024-07-05 22:12:08 +0530

    Final

--- main.py
+++ main.py
@@ -2,20 +2,32 @@ from dotenv import load_dotenv
 import os
 import streamlit as st
 from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import GooglePalmEmbeddings
-from langchain.vectorstores import Chroma
-from langchain.llms import GooglePalm
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_community.embeddings import GooglePalmEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_community.llms import GooglePalm
 from template import ai_chat, human_chat, css
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain import hub
+from langchain_google_genai import GoogleGenerativeAI,GoogleGenerativeAIEmbeddings
+
 
 # from langchain.memory import ConversationBufferMemory
 # from langchain.chains import ConversationalRetrievalChain
 
 
 # genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+# SYSTEM_PROMPT='''
+# You are an assistant for question-answering tasks.
+# Use the following pieces of retrieved context to answer the question.
+# If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
+# '''
+load_dotenv()
+my_api = os.getenv("GOOGLE_API_KEY")
+
+
 
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
@@ -40,13 +52,13 @@ def get_chunks(text):
 
 
 def get_vectordb(chunks):
-    embed = GooglePalmEmbeddings()
+    embed = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
     vectordb = Chroma.from_texts(texts=chunks,embedding=embed)
     return vectordb
     
 
 def get_chain(vectordb):
-    model = GooglePalm()
+    model = GoogleGenerativeAI(model="gemini-pro")
     # mem = ConversationBufferMemory(memory_key="chat_history",return_messages=True)
     # chain = ConversationalRetrievalChain.from_llm(
     #     llm=model,
@@ -70,7 +82,7 @@ def get_response(inp):
     st.write(ai_chat.replace("{{MSG}}",response),unsafe_allow_html=True)
 
 def main():
-    load_dotenv()
+    
     st.set_page_config(page_title="Multiple PDFs chatting",page_icon=":books:")
 
     st.write(css,unsafe_allow_html=True)
@@ -82,6 +94,7 @@ def main():
     inp = st.text_input("Ask question : ")
     if inp:
         get_response(inp)
+        
 
     # if st.button("Ask"):
     #     st.write(get_response(inp))
